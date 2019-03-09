84fb04c9cf9c43ad3de9f9aa45885fdb1e5aafb5
Kozhin Valeriy <35922956+jeanedit@users.noreply.github.com>
2019-03-09 20:09:30 +0300

    Ordered List

---
+++ OrderedList.cpp
@@ -0,0 +1,234 @@
+#include <stdio.h>
+#include <typeinfo.h>
+#include <string>
+
+using namespace std;
+
+
+template <class T>
+class Node
+{
+public:
+	T value;
+	Node* next;
+	Node* prev;
+
+	Node(T valueNode)
+	{
+		value = valueNode;
+		next = NULL;
+		prev = NULL;
+	}
+};
+
+template <class T>
+class OrderedList
+{
+private:
+	bool _ascending;
+
+public:
+	Node<T>* head;
+	Node<T>* tail;
+
+	OrderedList(bool asc)
+	{
+		head = NULL;
+		tail = NULL;
+		_ascending = asc;
+	}
+
+	int compare(T v1, T v2)
+	{
+		 if (typeid(T)==typeid(string)) //comparison of strings
+		{
+
+			string first;
+			string second;
+
+			int count = 0;
+
+			first.resize(v1.size());
+			second.resize(v2.size());
+
+
+			for (int i = 0; i < v1.size(); ++i)
+			{
+				if (v1[i] != ' ')
+					first[count++] = v1[i];
+			}
+
+			count = 0;
+
+			for (int i = 0; i < v2.size(); ++i)
+			{
+				if (v2[i] != ' ')
+					second[count++] = v2[i];
+			}
+
+			for (int i = 0; i < first.size(); ++i)
+			{
+				if (first[i] > second[i]) return 1;
+				else if (first[i] < second[i])return -1;
+			}
+
+			if (v1.size() < v2.size()) return 1;
+			if (v1.size() > v2.size()) return -1;
+			if (v1.size() == v2.size()) return 0;
+		}
+		else
+		{
+			if (v1 < v2)  return -1;
+			if (v1 == v2) return  0;
+			if (v1 > v2)  return  1;
+		}
+	}
+
+	void add(T value)
+	{
+		Node<T>*node = new Node<T>(value);
+
+		if (head == NULL)
+		{
+			head = node;
+			tail = node;
+			return;
+		}
+
+		Node<T>*curr = head;
+
+			while (curr != NULL)
+			{
+				int comp = compare(node->value, curr->value);
+	
+				if (_ascending&&(comp == 0 || comp == -1)
+					|| !_ascending&&(comp == 0 || comp == 1))
+				{
+					if (curr == head)
+					{
+						node->next = curr;
+						curr->prev = node;
+						head = node;
+						return;
+					}
+					else
+					{
+						node->prev = curr->prev;
+						node->next = curr;
+						curr->prev->next = node;
+						curr->prev = node;
+						return;
+					}
+				}
+				curr = curr->next;
+			}
+			node->prev = tail;
+			tail->next = node;
+			tail = node;
+			tail->next = NULL;
+			return;
+	}
+
+	Node<T>* find(T val)
+	{
+		Node<T>*node = head;
+		while (node != NULL)
+		{
+			if (_ascending)
+			{
+				if (compare(node->value, val) == 1)
+					return NULL;
+
+				if (node->value == val) return node;
+
+			}
+			else if (!_ascending)
+			{
+				if (compare(node->value, val) == -1)
+					return NULL;
+
+				if (node->value == val) return node;
+
+			}
+			else
+			{
+				return NULL;
+			}
+			node = node->next;
+		}
+		return NULL; 
+	}
+
+	void del(T val)
+	{
+		Node<T>*node = head;
+		if (head->value == val && tail->value == val)
+		{
+			head = NULL;
+			tail = NULL;
+			return;
+		}
+		if (node->value == val)
+		{
+			head = node->next;
+			return;
+		}
+		if (tail->value == val)
+		{
+			tail = tail->prev;
+			tail->next = NULL;
+			return;
+		}
+		while (node->value != val)
+		{
+			node = node->next;
+		}
+		if (node->prev)
+			node->prev->next = node->next;
+		if (node->next)
+			node->next->prev = node->prev;
+		return;
+	}
+
+	void clear(bool asc)
+	{
+		_ascending = asc;
+		Node<T>*node = head;
+		while (node != NULL)
+		{
+			head = node->next;
+			delete node;
+			node = head;
+		}
+		head = NULL;
+		tail = NULL;
+	}
+
+	int size()
+	{
+		Node<T>*node = head;
+		int count = 0;
+		
+		while (node != NULL)
+		{
+			count++;
+			node = node->next;
+		}
+		return count;
+	}
+
+
+	T* get_all()
+	{
+		Node*<T>node = head;
+		T*list = new T[size()];
+
+		for (int i = 0; node != NULL; ++i)
+		{
+			list[i] = node;
+			node = node->next;
+		}
+		return list;
+	}
+
+};
